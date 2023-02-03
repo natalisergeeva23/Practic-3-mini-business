8a95713e027c6be230443fead7cc58fc6b566ac6
Nataly Sergeeva <isip_n.a.sergeevna@mpt.ru>
2023-02-03 14:18:43 +0300

    2 Практическая

---
+++ Calendar.py
@@ -0,0 +1,18 @@
+print(f"Подсчтет дней в году")
+
+god = float(input("Введите год: ")) 
+Number = 0
+if god % 4 == 0:
+    Mas = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for kol in range(len(Mas)):
+        for Quantity in range(Mas[kol]):
+            Quantity += 1
+            Number += sum(map(int, str(Quantity)))
+    print(Number)
+else:
+    Massiv = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for kol in range(len(Massiv)):
+        for Quantity in range(Massiv[kol]):
+            Quantity += 1
+            Number += sum(map(int, str(Quantity)))   
+    print(Number)
